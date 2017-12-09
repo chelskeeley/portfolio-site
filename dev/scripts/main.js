b70e1a45e3aa465cd51ce653bc9611fd413b7843
Chelsea Keeley <chelsea.keeley@gmail.com>
2017-12-09 17:00:02 -0500

    styled mobile menu, finished responsive styling for header

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,9 +1,47 @@
 $(document).ready(function () {
 
 
+
     $('nav a').smoothScroll({
         speed: 1500,
         easing: 'swing'
     });
- 
+
+    // start of click transition for navicon
+    
+    
+    function mediaSize() {
+        if (window.matchMedia('(max-width: 940px)').matches) {
+            $('nav').addClass('mobile')
+            
+        } 
+        else {
+            /* Reset for CSS changes – Still need a better way to do this! */
+            $('nav').removeClass('mobile');
+            // $('li').removeClass('active');
+            // $('nav').removeClass('dim')
+        }
+    };
+    
+    /* Call the function */
+    mediaSize();
+    /* Attach the function to the resize event listener */
+    window.addEventListener('resize', mediaSize, false);
+    
+    $('.hamburger').click(function () {
+        $(this).toggleClass('open');
+        $('li').toggleClass('active');
+        $('nav').toggleClass('dim');
+
+    });
+
+
+    $('.mobile a').click(function () {
+        $('.hamburger').toggleClass('open');
+        $('li').removeClass('active');
+        $('nav').toggleClass('dim');
+
+    });
+
+
 });
